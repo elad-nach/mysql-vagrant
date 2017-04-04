c00fc00b5ad63759e0330c2253ec1251ab85c093
sheled <theman@elad.im>
2017-04-04 23:37:01 +0200

    back to xenial with mysql 5.6

--- Vagrantfile
+++ Vagrantfile
@@ -9,5 +9,5 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.network :forwarded_port, guest: 3306, host: 3306
   config.vm.provision :shell, :path => "install.sh"
   config.vm.synced_folder ".", "/vagrant", :mount_options => ["dmode=777", "fmode=666"]
-  config.vm.network "private_network", ip: "33.33.33.10", type: "dhcp", auto_config: false
+  config.vm.network "private_network", ip: "192.168.66.66", type: "dhcp", auto_config: false
 end
--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
+sudo add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty universe'
 sudo apt-get update
 sudo apt-get install -y vim curl python-software-properties
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 sudo apt-get update
-sudo apt-get install -y mysql-server
+sudo apt-get install -y mysql-server-5.6
 
-sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
+sudo sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
 sudo /etc/init.d/mysql restart
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
